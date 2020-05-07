695dfcecdada3cb54afc776898de99d60ad5e920
Benedikt Mandelkow <benedikt.mandelkow@rwth-aachen.de>
2020-05-07 10:47:48 +0200

    switch to using Cargo workspaces
    this removes the dependency on color backtrace from the library because
    I want to use this in a wasm environment
    
    see https://github.com/rust-lang/rfcs/pull/2887#issuecomment-614925031
    for why workspaces are the way to solve this

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ def fib(n):
         return fib(n-1) + fib(n-2)
 
 
-for i in range(20):
-    print(i+1, fib(i+1))
-# print(fib(25))
+# for i in range(20):
+#     print(i+1, fib(i+1))
+print(fib(36))
 
 
 def fac(n):
